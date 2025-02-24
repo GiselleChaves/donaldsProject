7e968cc32f7dbeca4e61e9e4d519c01da9652098
gisellechaves <gisellegoncalveschaves@hotmail.com>
2025-02-24 08:22:02 -0300

    feat: Creating router to back from menu screen to main screen

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -3,42 +3,41 @@
 import { Restaurant } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/router";
 
 interface ReataurantHeaderProps {
   restaurant: Pick<Restaurant, "name" | "coverImageUrl">;
 }
 
 const RestaurantHeader = ({ restaurant }: ReataurantHeaderProps) => {
-    const router = useRouter()
+  const router = useRouter();
   return (
-
     <div className="relative h-[250px] w-full">
-    <Button
-      variant="secondary"
-      size="icon"
-      className="absolute left-4 top-4 z-50 rounded-full"
-      onClick={() => router.back()}
-    >
-      <ChevronLeftIcon />
-    </Button>
-    
-    <Image
-      src={restaurant.coverImageUrl}
-      alt={restaurant.name}
-      className="object-cover"
-      fill
-    />
-    <Button
-      variant="secondary"
-      size="icon"
-      className="absolute right-4 top-4 rounded-full z-50"
-    >
-      <ScrollTextIcon />
-    </Button>
-  </div>
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={() => router.back()}
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      <Image
+        src={restaurant.coverImageUrl}
+        alt={restaurant.name}
+        className="object-cover"
+        fill
+      />
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 rounded-full z-50"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
   );
 };
 
